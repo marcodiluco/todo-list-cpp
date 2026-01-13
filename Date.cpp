059059372d2998a68651f3217625582e7639e20d
marco <pennimarco200@gmail.com>
2026-01-13 14:36:56 +0100

    creo funzione per verificare se hanno è bisestile e quella per vedere quanti giorni ci sono in un mese

--- Date.cpp
+++ Date.cpp
@@ -1,5 +1,35 @@
 #include Date.h
 
+bool Date::IsBisestile(int Year) const {
+    if (Year % 4 == 0) {
+        if (Year % 100 == 0) {
+            if (Year % 400 == 0)
+                return true;
+            else
+                return false;   //non bisestile perchè è divisibile per 100 e non per 400
+        } else
+            return true;
+    }
+    return false;            //non bisestile perchè non è divisibile per 4
+}
+int Date::DaysAvaible(int Month,int Year) const {
+    switch (Month){
+        case 1: case 3: case 5: case 7:
+        case 8: case 10: case 12:         //gennaio,marzo,maggio,luglio,agosto,ottobre,dicembre ne hanno 31
+            return 31;
+        case 4: case 6: case 9: case 11:    //aprile,giugno,settembre,novembre ne hanno 30
+            return 30;
+        case 2:
+            if (IsBisestile(Year)) {
+                return 29;
+            } else {
+                return 28;
+            }
+        default:
+            return 30;       // se qualcuno passa un numero maggiore di 12
+    }
+}
+
 int Date::GetDay() const {
     return Day;
 }
@@ -10,3 +40,6 @@ int Date::getYear() const {
     return Year;
 }
 
+void Date::SetDay(int Day){
+
+}
--- Date.h
+++ Date.h
@@ -6,12 +6,20 @@ private:
     int Day;
     int Month;
     int Year;
+
+    bool IsBisestile(int Year) const;
+    int DaysAvaible(int Month,int Year) const;
+
 public:
     Date() : Day(0),Month(0),Year(0){}
 
     int GetDay() const;
     int GetMonth() const;
     int GetYear() const;
+
+    void SetDay(int Day);
+    void SetMonth(int Month);
+    void SetYear(int Year);
 
 
 };
