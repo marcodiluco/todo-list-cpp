9e6093b11cf23d0d654b3e45413acb6126244e5b
marco <pennimarco200@gmail.com>
2026-01-17 13:25:56 +0100

    finisco classe coi test per le activity

--- TestActivity.cpp
+++ TestActivity.cpp
@@ -3,7 +3,7 @@
 #include <cassert>
 #include <iostream>
 
-void testActivityConstructor() {
+void TestActivityConstructor() {
     Date d;
     d.SetDay(15);
     d.SetMonth(3);
@@ -18,4 +18,37 @@ void testActivityConstructor() {
     assert(a.GetDate().GetYear() == 2026);
     // Controlla stato iniziale
     assert(a.IsComplete() == false);
+}
+void TestComplete() {
+    Date d;
+    Activity a("studiare", d);
+
+    a.SetComplete();
+    assert(a.IsComplete() == true);
+    a.SetUnComplete();
+    assert(a.IsComplete() == false);
+}
+void testSetDescription() {
+    Date d;
+    Activity a("Vecchia descrizione", d);
+
+    a.SetDescription("Nuova descrizione");
+    assert(a.GetDescription() == "Nuova descrizione");
+}
+void TestSetDate() {
+    Date d1;
+    d1.SetDay(1);
+    d1.SetMonth(1);
+    d1.SetYear(2026);
+    Activity a("Comprare pane", d1);
+
+    Date d2;
+    d2.SetDay(20);
+    d2.SetMonth(12);
+    d2.SetYear(2026);
+    a.SetDate(d2);
+
+    assert(a.GetDate().GetDay() == 20);
+    assert(a.GetDate().GetMonth() == 12);
+    assert(a.GetDate().GetYear() == 2026);
 }
