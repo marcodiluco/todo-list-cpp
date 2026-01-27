1504d04bc60dc9f2a0c2bef1d6dac0b77a531b94
marco <pennimarco200@gmail.com>
2026-01-27 13:55:09 +0100

    inizio test del date

--- ToDoListTests.cpp
+++ ToDoListTests.cpp
@@ -1,1 +1,11 @@
+#include "gtest/gtest.h"
+#include "Date.h"
 
+// Test Date
+
+TEST(DateTest,DefaultConstructor){
+    Date d;
+    EXPECT_EQ(d.GetDay(), 1);
+    EXPECT_EQ(d.GetMonth(), 1);
+    EXPECT_EQ(d.GetYear(), 2000);
+}
---
+++ cmake-build-debug/ToDoListTests[1]_tests.cmake
@@ -0,0 +1,3 @@
+add_test([=[DateTest.DefaultConstructor]=]  C:/Users/39345/OneDrive/Desktop/ToDoList/cmake-build-debug/ToDoListTests.exe [==[--gtest_filter=DateTest.DefaultConstructor]==] --gtest_also_run_disabled_tests)
+set_tests_properties([=[DateTest.DefaultConstructor]=]  PROPERTIES WORKING_DIRECTORY C:/Users/39345/OneDrive/Desktop/ToDoList/cmake-build-debug SKIP_REGULAR_EXPRESSION [==[\[  SKIPPED \]]==])
+set(  ToDoListTests_TESTS DateTest.DefaultConstructor)
